84453b64b4d1a84ad30990cb1e7004612075c718
mr-lu1s23 <llope32@lsu.edu>
2025-02-25 22:23:52 -0600

    Changed grand piano sound with system sound

--- build_a_sampler/sketch.js
+++ build_a_sampler/sketch.js
@@ -1,11 +1,11 @@
 let samples;
 let button1, button2, button3, button4;
-let del = new Tone.FeedbackDelay(0.5, 0.5).toDestination();
+let del = new Tone.FeedbackDelay(0, 0.5).toDestination();
 
 function preload(){
   samples = new Tone.Players({
     boxed_tissue: "media/boxed-tissue-pull-4.wav",
-    grand_piano: "media/grand-piano-dang-dang.wav",
+    system_sound: "media/system-sound-interval-m3.wav",
     keychron: "media/keychron-k10-space_bar.wav",
     pan_hit: "media/pan-hit.wav"
   }).connect(del);
@@ -15,15 +15,15 @@ function setup() {
   createCanvas(400, 400);
   textSize(16);
   button1 = createButton("Boxed Tissue");
-  button1.position(10, 20);
-  button2 = createButton("Grand Piano Dang-Dang");
-  button2.position(200, 20);
+  button1.position(10, 23);
+  button2 = createButton("System Sound");
+  button2.position(200, 23);
   button3 = createButton("Keychron K10 Space Bar");
-  button3.position(10, 45);
+  button3.position(10, 48);
   button4 = createButton("Pan Hit");
-  button4.position(200, 45);
+  button4.position(200, 48);
   button1.mousePressed(() => {samples.player("boxed_tissue").start()});
-  button2.mousePressed(() => {samples.player("grand_piano").start()});
+  button2.mousePressed(() => {samples.player("system_sound").start()});
   button3.mousePressed(() => {samples.player("keychron").start()});
   button4.mousePressed(() => {samples.player("pan_hit").start()});
   delTimeSlider = createSlider(0, 1, 0, 0.01);
