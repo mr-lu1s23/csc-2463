314108fa507021eb3f275204886ac211a9a07399
mr-lu1s23 <llope32@lsu.edu>
2025-03-06 23:42:44 -0600

    Created slider for reverb

--- build_a_synth/sketch.js
+++ build_a_synth/sketch.js
@@ -1,4 +1,5 @@
-let filter, effect, synth;
+let filter, effect, synth, polySynth, rev;
+let activeKey = null;
 let keynotes = {
   'a' : 'C5',
   's' : 'D5',
@@ -14,21 +15,28 @@ function setup() {
   createCanvas(400, 400);
   filter = new Tone.Filter(2000, "lowpass").toDestination();
   effect = new Tone.Reverb(3).connect(filter);
-  synth = new Tone.PolySynth({
+  synth = new Tone.PolySynth().connect(effect);
+  synth.set({
     envelope: {
-      attack : 0.1,
+      attack : 0.9,
       decay : 0.2,
-      sustain : 0.9,
-      release : 0.2,
+      sustain : 0.7,
+      release : 1.5,
     }
-  }).connect(effect);
+  })
   textSize(14);
+  rev = createSlider(1, 5, 3, 1);
+  rev.position(10, 80);
+  rev.input(() => {effect.set({
+    decay : rev.value()
+  })});
 }
 
 function draw() {
   background(220);
   text('Simple Web Synthesizer', 10, 20);
   text('Use [A] [S] [D] [F] [G] [H] [J] [K] to play notes', 10, 40);
+  text('Reverb: ' + rev.value(), 10, 70);
 }
 
 function keyPressed() {
